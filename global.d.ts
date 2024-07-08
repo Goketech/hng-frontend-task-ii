4cb6cf167194ef5822090110fcf15d782dec0e77
Modupe Akanni <modupe775@gmail.com>
2024-07-08 01:49:47 +0100

    update element type

---
+++ global.d.ts
@@ -0,0 +1,12 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+      'swiper-container': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        navigation: string;
+        // Add more properties
+      };
+      'swiper-slide': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        lazy: string;
+        // Add more properties
+      };
+    }
+  }
