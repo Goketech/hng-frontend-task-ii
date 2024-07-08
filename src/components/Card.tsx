7d4e8b78cd895d0ddbd1c3a38b6aa5eebd7c98f8
Modupe Akanni <modupe775@gmail.com>
2024-07-09 00:28:15 +0100

    route product cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 import Image from "next/image";
+import Link from 'next/link';
 
 interface ProductCardProps {
     discountPercentage: string;
@@ -18,11 +19,12 @@ const Card: React.FC<ProductCardProps> = ({ discountPercentage,
     productImage,
     numberOfRatings, oldPrice, newPrice }) => {
     return (
-        <motion.div whileHover={{ scale: 1.05 }}
-            transition={{ type: 'spring', stiffness: 300, damping: 20 }} className='p-2 border rounded-lg md:mr-6 cursor-pointer'>
-            <div className='flex justify-between items-center h-auto'>
-                <p className='bg-[#DB4444] px-3 py-1 rounded-lg text-white'>{discountPercentage}</p>
-                {/* <Image
+        <Link href="/productpage">
+            <motion.div whileHover={{ scale: 1.05 }}
+                transition={{ type: 'spring', stiffness: 300, damping: 20 }} className='p-2 border rounded-lg md:mr-6 cursor-pointer'>
+                <div className='flex justify-between items-center h-auto'>
+                    <p className='bg-[#DB4444] px-3 py-1 rounded-lg text-white'>{discountPercentage}</p>
+                    {/* <Image
                     className=""
                     src="/Wishlist.svg"
                     alt="wishlist"
@@ -30,66 +32,67 @@ const Card: React.FC<ProductCardProps> = ({ discountPercentage,
                     height={32}
                     priority
                 /> */}
-            </div>
-            <div className='flex justify-center items-center h-72 mt-14'>
-                <Image
-                    src={productImage}
-                    alt="product"
-                    width={300}
-                    height={300}
-                />
-            </div>
-            <div className='flex justify-between mt-8 mb-2'>
-                <p className='font-bold'>{title}</p>
-                <div className='rounded-full bg-[#FF8933] p-2'>
+                </div>
+                <div className='flex justify-center items-center h-72 mt-14'>
                     <Image
-                        src="/white-shop.svg"
-                        alt="shop"
-                        width={32}
-                        height={32}
+                        src={productImage}
+                        alt="product"
+                        width={300}
+                        height={300}
                     />
                 </div>
-            </div>
-            <div className='mb-2'>
-                <span className='text-[#DB4444] font-medium mr-2'>${newPrice}</span>
-                <span className='text-[#808080] font-medium line-through'> ${oldPrice}</span>
-            </div>
-            <div className='flex gap-2'>
-                <div className='flex md:gap-1'>
-                    <Image
-                        src="/full-star.svg"
-                        alt="star"
-                        width={20}
-                        height={20}
-                    />
-                    <Image
-                        src="/full-star.svg"
-                        alt="star"
-                        width={20}
-                        height={20}
-                    />
-                    <Image
-                        src="/full-star.svg"
-                        alt="star"
-                        width={20}
-                        height={20}
-                    />
-                    <Image
-                        src="/full-star.svg"
-                        alt="star"
-                        width={20}
-                        height={20}
-                    />
-                    <Image
-                        src="/full-star.svg"
-                        alt="star"
-                        width={20}
-                        height={20}
-                    />
+                <div className='flex justify-between mt-8 mb-2'>
+                    <p className='font-bold'>{title}</p>
+                    <div className='rounded-full bg-[#FF8933] p-2'>
+                        <Image
+                            src="/white-shop.svg"
+                            alt="shop"
+                            width={32}
+                            height={32}
+                        />
+                    </div>
                 </div>
-                <p className='text-[#808080] font-medium'>({numberOfRatings})</p>
-            </div>
-        </motion.div>
+                <div className='mb-2'>
+                    <span className='text-[#DB4444] font-medium mr-2'>${newPrice}</span>
+                    <span className='text-[#808080] font-medium line-through'> ${oldPrice}</span>
+                </div>
+                <div className='flex gap-2'>
+                    <div className='flex md:gap-1'>
+                        <Image
+                            src="/full-star.svg"
+                            alt="star"
+                            width={20}
+                            height={20}
+                        />
+                        <Image
+                            src="/full-star.svg"
+                            alt="star"
+                            width={20}
+                            height={20}
+                        />
+                        <Image
+                            src="/full-star.svg"
+                            alt="star"
+                            width={20}
+                            height={20}
+                        />
+                        <Image
+                            src="/full-star.svg"
+                            alt="star"
+                            width={20}
+                            height={20}
+                        />
+                        <Image
+                            src="/full-star.svg"
+                            alt="star"
+                            width={20}
+                            height={20}
+                        />
+                    </div>
+                    <p className='text-[#808080] font-medium'>({numberOfRatings})</p>
+                </div>
+            </motion.div>
+        </Link>
     )
 }
 
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from 'framer-motion';
 import Image from "next/image";
+import Link from 'next/link';
 
 interface CardProps {
     title: string;
@@ -16,9 +17,10 @@ const ProductCard: React.FC<CardProps> = ({
     productImage,
     numberOfRatings, oldPrice, newPrice }) => {
     return (
-        <motion.div whileHover={{ scale: 1.05 }}
-            transition={{ type: 'spring', stiffness: 300, damping: 20 }} className='border relative rounded-lg md:mr-6 cursor-pointer'>
-            {/* <div className='p-4 mb-4 absolute right-4'>
+        <Link href="/productpage">
+            <motion.div whileHover={{ scale: 1.05 }}
+                transition={{ type: 'spring', stiffness: 300, damping: 20 }} className='border relative rounded-lg md:mr-6 cursor-pointer'>
+                {/* <div className='p-4 mb-4 absolute right-4'>
                 <Image
                     className=""
                     src="/Wishlist.svg"
@@ -28,67 +30,68 @@ const ProductCard: React.FC<CardProps> = ({
                     priority
                 />
             </div> */}
-            <div className='flex justify-center items-center h-72 md:mt-14 md:mb-10'>
-                <Image
-                    src={productImage}
-                    alt="product"
-                    width={300}
-                    height={300}
-                />
-            </div>
-            <div className='bg-[#F5F5F5] pt-px'>
-                <div className='flex justify-between mt-8 mb-2 px-4'>
-                    <p className='font-bold'>{title}</p>
-                    <div className='rounded-full bg-[#FF8933] p-2'>
-                        <Image
-                            src="/white-shop.svg"
-                            alt="shop"
-                            width={32}
-                            height={32}
-                        />
+                <div className='flex justify-center items-center h-72 md:mt-14 md:mb-10'>
+                    <Image
+                        src={productImage}
+                        alt="product"
+                        width={300}
+                        height={300}
+                    />
+                </div>
+                <div className='bg-[#F5F5F5] pt-px'>
+                    <div className='flex justify-between mt-8 mb-2 px-4'>
+                        <p className='font-bold'>{title}</p>
+                        <div className='rounded-full bg-[#FF8933] p-2'>
+                            <Image
+                                src="/white-shop.svg"
+                                alt="shop"
+                                width={32}
+                                height={32}
+                            />
+                        </div>
+                    </div>
+                    <div className='mb-2 px-4'>
+                        <span className='text-[#DB4444] font-medium mr-2'>${newPrice}</span>
+                        <span className='text-[#808080] font-medium line-through'> ${oldPrice}</span>
+                    </div>
+                    <div className='flex gap-2 px-4 pb-4'>
+                        <div className='flex md:gap-1'>
+                            <Image
+                                src="/full-star.svg"
+                                alt="star"
+                                width={20}
+                                height={20}
+                            />
+                            <Image
+                                src="/full-star.svg"
+                                alt="star"
+                                width={20}
+                                height={20}
+                            />
+                            <Image
+                                src="/full-star.svg"
+                                alt="star"
+                                width={20}
+                                height={20}
+                            />
+                            <Image
+                                src="/full-star.svg"
+                                alt="star"
+                                width={20}
+                                height={20}
+                            />
+                            <Image
+                                src="/half-star.svg"
+                                alt="star"
+                                width={20}
+                                height={20}
+                            />
+                        </div>
+                        <p className='text-[#808080] font-medium'>({numberOfRatings})</p>
                     </div>
                 </div>
-                <div className='mb-2 px-4'>
-                    <span className='text-[#DB4444] font-medium mr-2'>${newPrice}</span>
-                    <span className='text-[#808080] font-medium line-through'> ${oldPrice}</span>
-                </div>
-                <div className='flex gap-2 px-4 pb-4'>
-                    <div className='flex md:gap-1'>
-                        <Image
-                            src="/full-star.svg"
-                            alt="star"
-                            width={20}
-                            height={20}
-                        />
-                        <Image
-                            src="/full-star.svg"
-                            alt="star"
-                            width={20}
-                            height={20}
-                        />
-                        <Image
-                            src="/full-star.svg"
-                            alt="star"
-                            width={20}
-                            height={20}
-                        />
-                        <Image
-                            src="/full-star.svg"
-                            alt="star"
-                            width={20}
-                            height={20}
-                        />
-                        <Image
-                            src="/half-star.svg"
-                            alt="star"
-                            width={20}
-                            height={20}
-                        />
-                    </div>
-                    <p className='text-[#808080] font-medium'>({numberOfRatings})</p>
-                </div>
-            </div>
-        </motion.div>
+            </motion.div>
+        </Link>
     )
 }
 
